4e35dd910c98f7f2a622474fdb3d1f9e46c3277e
Lees <thomas.lees@tylertech.com>
2017-10-05 15:00:30 -0600

    Placeholders!

---
+++ security_manager/app/components/application/application.ts
@@ -0,0 +1,10 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+    selector: 'security-manager',
+    templateUrl: 'application.html',
+    styleUrls: ['application.css'],
+    encapsulation:ViewEncapsulation.None
+})
+
+export default class ApplicationComponent {};
